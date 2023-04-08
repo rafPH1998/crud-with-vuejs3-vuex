dfabf0f1e2cce28962deb4e0a2950b576771227c
Rafael Belchior <rafaelb89.rb@gmail.com>
2023-04-08 19:31:43 -0300

    definindo rotas das telas

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import IndexView from '@/views/Users/IndexView.vue'
+import CreateView from '@/views/Users/CreateView.vue'
+import EditView from '@/views/Users/EditView.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/users',
+    name: 'users.index',
+    component: IndexView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    path: '/users/create',
+    name: 'users.add',
+    component: CreateView
+  },
+  {
+    path: '/users/:id',
+    name: 'users.edit',
+    component: EditView,
+    props: true
+  },
+]  
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
