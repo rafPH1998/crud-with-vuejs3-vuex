26dd86fccfde9f5004620ac70ed235ccfd85d7f4
Rafael Belchior <rafaelb89.rb@gmail.com>
2023-04-08 19:31:04 -0300

    configurando main com axios e URL padrao

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import '@/assets/css/main.css'
+import axios from 'axios'
+import {URL_API} from './configs/api'
 
-createApp(App).use(store).use(router).mount('#app')
+axios.defaults.baseURL = URL_API
+
+createApp(App)
+    .use(router)
+    .mount('#app')
